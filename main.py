2efd17ec6c78cd388033534d2c7d57004ff6f7f7
qmet1324 <mikhail.melnik.gamedev@gmail.com>
2025-04-28 11:34:05 -0400

    feat: restrict fps to 60

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from constants import *
 
 def main():
     pygame.init()
+    fps_counter = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     # infinite game loop
     while True:
@@ -11,6 +13,8 @@ def main():
                 return 
         pygame.Surface.fill(screen, BLACK_COLOR)
         pygame.display.flip()
+        fps_counter.tick(60)
+        dt = fps_counter.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
